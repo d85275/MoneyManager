37b46aa6c88483225c85881328349a2b4044571c
Chao <d85275@gmail.com>
2020-09-28 00:43:33 +0800

    editting history page

--- app/src/main/java/com/example/demo/HistoryActivity.kt
+++ app/src/main/java/com/example/demo/HistoryActivity.kt
@@ -1,12 +1,15 @@
 package com.example.demo
 
 import android.os.Bundle
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.github.sundeepk.compactcalendarview.CompactCalendarView.CompactCalendarViewListener
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_history.*
 import java.text.SimpleDateFormat
@@ -15,23 +18,26 @@ import java.util.*
 class HistoryActivity : AppCompatActivity() {
     private val dateFormatForMonth: SimpleDateFormat =
         SimpleDateFormat("MMM - yyyy", Locale.getDefault())
+    private var isShow = true
+    private var scrollRange = -1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
         setSupportActionBar(toolbar)
-        var isShow = true
-        var scrollRange = -1
+        tvCurrentDate.text = dateFormatForMonth.format(compactcalendar_view.firstDayOfCurrentMonth)
+        setListeners()
         appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { barLayout, verticalOffset ->
-            if (scrollRange == -1){
+            if (scrollRange == -1) {
                 scrollRange = barLayout?.totalScrollRange!!
             }
-            if (scrollRange + verticalOffset == 0){
-                collapsingToolbar.title =dateFormatForMonth.format(compactcalendar_view.getFirstDayOfCurrentMonth())
-
+            if (scrollRange + verticalOffset == 0) {
+                collapsingToolbar.title =
+                    dateFormatForMonth.format(compactcalendar_view.firstDayOfCurrentMonth)
                 isShow = true
-            } else if (isShow){
-                collapsingToolbar.title = " " //careful there should a space between double quote otherwise it wont work
+            } else if (isShow) {
+                collapsingToolbar.title =
+                    " " //careful there should a space between double quote otherwise it wont work
                 isShow = false
             }
         })
@@ -40,13 +46,27 @@ class HistoryActivity : AppCompatActivity() {
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
+    private fun setListeners() {
+        compactcalendar_view.setListener(object : CompactCalendarViewListener {
+            override fun onDayClick(dateClicked: Date?) {
+                tvCurrentDate.text = dateFormatForMonth.format(dateClicked)
+            }
+
+            override fun onMonthScroll(firstDayOfNewMonth: Date?) {
+                tvCurrentDate.text = dateFormatForMonth.format(firstDayOfNewMonth)
+            }
+        })
+    }
+
+
 }
 
-class SimpleRecyclerAdapter(private val items: List<String>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class SimpleRecyclerAdapter(private val items: List<String>) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(parent)
 
-    override fun onBindViewHolder(holder:RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         (holder as ViewHolder).bind(items[position])
     }
 
