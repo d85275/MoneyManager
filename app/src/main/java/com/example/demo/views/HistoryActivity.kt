21febe1b678909ecb1b47b486c612655d5b7e803
Chao <d85275@gmail.com>
2020-09-29 23:19:18 +0800

    add buttons for navigating to previous and next month

--- app/src/main/java/com/example/demo/views/HistoryActivity.kt
+++ app/src/main/java/com/example/demo/views/HistoryActivity.kt
@@ -1,13 +1,12 @@
 package com.example.demo.views
 
+import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,8 +14,6 @@ import com.example.demo.R
 import com.example.demo.viewmodels.HistoryViewModel
 import com.google.android.material.appbar.AppBarLayout
 import kotlinx.android.synthetic.main.activity_history.*
-import java.text.SimpleDateFormat
-import java.util.*
 
 class HistoryActivity : AppCompatActivity() {
     private var isShow = true
@@ -27,50 +24,59 @@ class HistoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
         setSupportActionBar(toolbar)
-        //tvCurrentDate.text = dateFormatForMonth.format(compactcalendar_view.firstDayOfCurrentMonth)
+        getViewModel()
         setListeners()
         initViews()
-        getViewModel()
         initObservers()
     }
 
-    private fun initViews(){
+    private fun initViews() {
+        collapsingToolbar.title = " "
+        tvCurrentDate.text = viewModel.getDate(compactcalendar_view.firstDayOfCurrentMonth)
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter =
             SimpleRecyclerAdapter((1..30).map { "Item $it" })
         recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
-    private fun getViewModel(){
+    private fun getViewModel() {
         viewModel = ViewModelProvider(this).get(HistoryViewModel::class.java)
     }
 
-    private fun initObservers(){
-        viewModel.selectedDay.observe(this, androidx.lifecycle.Observer { day->
+    private fun initObservers() {
+        viewModel.selectedDay.observe(this, androidx.lifecycle.Observer { day ->
             val selectedDay = viewModel.getDay(day)
-            Log.e(javaClass.name,"selected day: $selectedDay")
+            Log.e(javaClass.name, "selected day: $selectedDay")
         })
 
-        viewModel.selectedMonth.observe(this, androidx.lifecycle.Observer { firstDayOfMonth->
+        viewModel.selectedMonth.observe(this, androidx.lifecycle.Observer { firstDayOfMonth ->
             tvCurrentDate.text = viewModel.getDate(firstDayOfMonth)
         })
     }
 
     private fun setListeners() {
+        Log.e("his","set listeners")
         compactcalendar_view.setListener(viewModel.getCalendarListener())
         appbar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { barLayout, verticalOffset ->
             if (scrollRange == -1) {
                 scrollRange = barLayout?.totalScrollRange!!
             }
             if (scrollRange + verticalOffset == 0) {
-                collapsingToolbar.title =viewModel.getDate(compactcalendar_view.firstDayOfCurrentMonth)
+                tvCurrentDate.setTextColor(Color.BLACK)
+                ivPreMonth.setImageResource(R.drawable.pre_arrow_black)
+                ivNextMonth.setImageResource(R.drawable.next_arrow_black)
                 isShow = true
             } else if (isShow) {
-                collapsingToolbar.title =
-                    " " //careful there should a space between double quote otherwise it wont work
+                tvCurrentDate.setTextColor(Color.WHITE)
+                ivPreMonth.setImageResource(R.drawable.pre_arrow_white)
+                ivNextMonth.setImageResource(R.drawable.next_arrow_white)
                 isShow = false
             }
         })
+
+        ivPreMonth.setOnClickListener {
+            compactcalendar_view.scrollLeft() }
+        ivNextMonth.setOnClickListener { compactcalendar_view.scrollRight() }
     }
 
 
